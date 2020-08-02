ab8cb95487c2c1b74992a3b36f5eebe0c2aa58f7
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-08-01 19:59:05 -0600

    inicio cod2

--- T01POS/t01pos.v
+++ T01POS/t01pos.v
@@ -1,7 +1,34 @@
-hola mundo
-adoal
-module t01pos
-enmodule
-para comer
-necesito pizza
-y hamburguesas
+
+module t01pos();
+
+  wire out0,out1,out2,out3,out4,out5,out6;
+  reg A,B,C;
+
+  not G4(out4,A);
+  not G5(out5,B);
+  not G6(out6,C);
+  or  G1(out1,A,B,out6);
+  or  G2(out2,A,out5,out6);
+  or  G3(out3,out4,out5,C);
+  and G0(out0,out1,out2,out3);
+
+  initial begin
+    $display("A B C | Y");
+    $display("------|--");
+    $monitor("%b %b %b | %b", A, B, C, out0);
+    A = 0; B = 0; C = 0;
+    #1 C = 1;
+    #1 B = 1; C = 0;
+    #1 B = 1; C = 1;
+    #1 A = 1; B = 0; C = 0;
+    #1 A = 1; B = 0; C = 1;
+    #1 A = 1; B = 1; C = 0;
+    #1 A = 1; B = 1; C = 1;
+    #1 $finish;
+  end
+
+  initial begin
+    $dumpfile("t01pos_tb.vcd");
+    $dumpvars(0, t01pos);
+  end
+endmodule
