7d071ba3a2d95f0206f34f5b50492d7f620ec5ba
fue19389 <68131348+fue19389@users.noreply.github.com>
2020-08-01 18:13:27 -0600

    gates

--- T01POS/t01pos.v
+++ T01POS/t01pos.v
@@ -1,4 +1,13 @@
-hola mundo
-adoal
-dd
-guardar 
+module t01pos();
+
+  wire out0,out1,out2,out3,out4,out5,out6;
+  reg A,B,C,D;
+
+  not G4(out4,A);
+  not G5(out5,B);
+  not G6(out6,C);
+  or  G1(out1,A,B,out6);
+  or  G2(out2,A,out5,out6);
+  or  G3(out3,out4,out5,C);
+  and G0(out0,out1,out2,out3);
+  
